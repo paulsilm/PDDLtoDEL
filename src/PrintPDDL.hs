5c09a7c1e64e630437fcd5d0d3d5f17972894993
paulsilm <paul.silm99@gmail.com>
2021-04-02 03:32:25 +0300

    finished printing requirements, types and predicates

--- src/PrintPDDL.hs
+++ src/PrintPDDL.hs
@@ -6,12 +6,20 @@ import PDDL
 ppDomain :: CheckDomain -> String
 ppDomain (CheckDomain str reqs strs preds actions) = 
      "(define (domain " ++ str ++ ")\n" 
-  ++ "\t(:requirements" ++ (foldr (\r s -> s ++ " " ++ (ppReq r)) "" reqs) ++ ")\n"
-  -- ++ "\t(:types" ++ (foldr (\s -> " " ++ s) "" strs) ++ ")\n"
-  -- ++ "\t(predicates" ++ (ppPreds preds) ++ ")\n"
+  ++ "\t(:requirements" ++ (concat $ map (\r -> " " ++ ppReq r) reqs) ++ ")\n"
+  ++ "\t(:types" ++ (concat $ map (" " ++) strs) ++ ")\n"
+  ++ "\t(predicates" ++ (concat $ map (\p -> "\n\t\t" ++ ppPred p) preds) ++ "\n\t)\n"
   -- ++ foldr ppAction "\n" actions 
   ++ ")"
 
 ppReq :: Req -> String 
 ppReq Strips = ":strips"
 ppReq Typing = ":typing"
+
+ppPred :: Predicate -> String 
+ppPred (PredAtom a) = "(" ++ a ++ ")"
+ppPred (PredSpec p vars) = "(" ++  p ++ (concat $ map (" " ++) vars) ++ ")"
+ppPred (PredDef p varTypes) = "(" ++  p ++ (concat $ map (\v -> " " ++ ppVars v) varTypes) ++ ")"
+
+ppVars :: VarType -> String
+ppVars (VTL vars objType) = (concat $ map (" ?" ++) vars) ++ " - " ++ objType
