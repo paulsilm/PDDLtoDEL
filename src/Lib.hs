dca3621272eb9b83d1d5c3aaa25162aef0d307ee
paulsilm <paul.silm99@gmail.com>
2021-04-14 14:00:12 +0300

    added a translation function for ppFormWith

---
+++ src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib where
+
+import SMCDEL.Language
+import PDDL 
+import PrintPDDL (ppPred)
+import SMCDEL.Internal.Help ((!))
+import Data.Tuple (swap)
+
+translationForPP :: [(Predicate, Prp)] -> (Prp -> String)
+translationForPP atomMap = (\p -> ppPred $ (map swap atomMap) ! p)
