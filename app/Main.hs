3ea20bf62c90c2c669f2ba08f4267d4b3bd986b8
paulsilm <paul.silm99@gmail.com>
2021-05-05 16:04:26 +0300

    added cli options for maximum depth of search tree, whether the solution should be implicitly coordinated or not and possibility to add domain and problem separately

--- app/Main.hs
+++ app/Main.hs
@@ -8,26 +8,32 @@ import PDDL
 import System.Exit (exitFailure)
 import SemanticChecker
 import SMCDEL.Internal.TexDisplay
-import SMCDEL.Other.Planning
+import Plan
 import System.Environment (getArgs,getProgName)
 import System.IO (hPutStrLn, stderr)
 
-
 main :: IO ()
 main = do
-  input <- getInput
-  --input <- readFile fileName
-  case parse $ alexScanTokens input of
-      Left (lin,col) -> error ("Parse error in line " ++ show lin ++ ", column " ++ show col)
-      Right pddl -> do
-        case validInput pddl of
-          Nothing -> do
-            putStrLn "Succesful parsing"
-            --putStrLn $ show $ pddlToDEL pddl
-            --writeFile fileName $ ppInput pddl --Useful for formatting the file, but loses comments
-            putStrLn $ findShortestICPlan $ pddl
-          Just str -> putStrLn $ str ++ show pddl
-
+  input <- getInput 
+  case input of
+    Right (domFile,prbFile,depth,icSolves) -> do
+      domain <- readFile domFile
+      problem <- readFile prbFile
+      let splitIndex = length $ lines domain
+      let contents = domain ++ "\n" ++ problem
+      case parse $ alexScanTokens contents of
+          Left (lin,col) -> do
+            let fileT = if lin < splitIndex then "domain" else "problem"
+            let lineNo = if lin < splitIndex then lin else lin - splitIndex
+            error ("Parse error in the " ++ fileT ++ " file in line " ++ show lineNo ++ ", column " ++ show col)
+          Right pddl -> do
+            processPDDL pddl depth icSolves
+    Left (filename,depth,icSolves) -> do
+      contents <- readFile filename
+      case parse $ alexScanTokens contents of
+          Left (lin,col) -> error ("Parse error in line " ++ show lin ++ ", column " ++ show col)
+          Right pddl -> do
+            processPDDL pddl depth icSolves
         {- let (actionModelMap,problem) = pddlToDEL pddl
         disp $ (map snd) actionModelMap
         putStrLn $ tex problem
@@ -37,35 +43,71 @@ main = do
         print domain
         print problem -}
 
-findShortestICPlan :: PDDL -> String
-findShortestICPlan pddl = 
-  head [concatMap ppICPlan plan 
-        | plan <- [findSequentialIcPlan i $ pddlToDEL pddl | i <- [1..]] -- plan :: [[ICPlan Action]]
-        , plan /= []] -- :: [[String]]
-    
+--A function just for stuff
+processPDDL :: PDDL -> Int -> Bool -> IO ()
+processPDDL pddl depth icSolves = 
+  case validInput pddl of
+    Just str -> do
+      putStrLn $ str ++ show pddl -- print error messages
+    Nothing -> do
+      putStrLn "Succesful parsing"
+      --putStrLn $ show $ pddlToDEL pddl
+      --writeFile fileName $ ppInput pddl --Useful for formatting the file, but loses comments
+      putStrLn $ findShortestICPlan pddl icSolves depth
 
-getInput :: IO String
+--Returns either (filecontents, depth, icSolves) or (domain, problem, depth, icSolves)
+getInput :: IO (Either (String, Int, Bool) (String, String, Int, Bool))
 getInput = do
   args <- getArgs
-  case args of
-    ("-dom":domfile:"-prb":problemfile:[]) -> do
-      dom <- readFile domfile
-      problem <- readFile problemfile
-      let input = dom ++ problem
-      return input
-    ([filename]) -> do
-      input <- readFile filename
-      return input
-    _ -> do
+  let input = inputFromArgs (Left ("", 0, False)) args
+  case input of
+    Just contents -> do
+      return contents
+    Nothing -> do
       name <- getProgName
       mapM_ (hPutStrLn stderr)
         [ infoline
-        , "usage: " ++ name ++ " <filename> {options}"
-        , "       (use filename - for STDIN)\n"
+        , "usage: " ++ name ++ " <filename> | <options>\n"
+        --, "  (use filename - for STDIN)\n"
         , "  -dom <filename>   parse the domain file\n"
-        , "  -pr  <filename>   parse the problem file\n" 
+        , "  -prb <filename>   parse the problem file\n" 
+        , "  -d   <int>        the maximum depth of solution\n" 
+        , "  -ic               constrain actions to those where the agent knows it will lead to the goal\n" 
         ]
       exitFailure
+
+--TODO? add - for stdin
+--Returns filenames and stuff
+inputFromArgs :: Either (String, Int, Bool) (String, String, Int, Bool) -> [String] -> Maybe (Either (String, Int, Bool) (String, String, Int, Bool))
+inputFromArgs (Left (fn, d, b)) [] 
+  | fn /= "" = Just (Left (fn, d, b))
+  | otherwise = Nothing
+inputFromArgs (Right (dom, prb, d, b)) [] 
+  | dom /= "" && prb /= "" = Just (Right (dom, prb, d, b))
+  | otherwise = Nothing
+inputFromArgs (Left (fn, _, b)) ("-d":i:args) = 
+  inputFromArgs (Left (fn, read i :: Int, b)) args
+inputFromArgs (Right (df, pf, _, b)) ("-d":i:args) = 
+  inputFromArgs (Right (df, pf, read i :: Int, b)) args
+inputFromArgs (Left (fn, d, b)) ("-dom":dom:args) 
+  | fn /= "" = Nothing 
+  | otherwise = inputFromArgs (Right (dom, "", d, b)) args
+inputFromArgs (Right (fn, prb, d, b)) ("-dom":dom:args) 
+  | fn /= "" = Nothing
+  | otherwise = inputFromArgs (Right (dom, prb, d, b)) args
+inputFromArgs (Left (fn, d, b)) ("-prb":prb:args)
+  | fn /= "" = Nothing 
+  | otherwise = inputFromArgs (Right ("", prb, d, b)) args
+inputFromArgs (Right (dom, fn, d, b)) ("-prb":prb:args) 
+  | fn /= "" = Nothing
+  | otherwise = inputFromArgs (Right (dom, prb, d, b)) args
+inputFromArgs (Left (fn, d, _)) ("-ic":args) = inputFromArgs (Left (fn, d, True)) args
+inputFromArgs (Right (dom, prb, d, _)) ("-ic":args) = inputFromArgs (Right (dom, prb, d, True)) args
+inputFromArgs (Left (fo, d, b)) (fn:args) 
+  | fo /= "" = Nothing
+  | otherwise = inputFromArgs (Left (fn, d, b)) args
+inputFromArgs (Right _) (_:_) = Nothing
+--inputFromArgs _ _ = Nothing
 
 infoline :: String
 infoline = "PDDLtoDEL " ++ "1.0.0" ++ " -- https://github.com/paulsilm/PDDLtoDEL\n"
